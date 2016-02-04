805bc196f9bfe9519fe70d549dc25f7444441702
isiherge <skull_caz0412@hotmail.com>
2016-02-04 14:22:10 -0600

    se hiso una nueva vista

---
+++ entero.py
@@ -0,0 +1,33 @@
+#e = 5 #El Tipado de variables es dinamico por lo que no nos causa problema lo siguiente
+#e = 5L
+
+real = 0.445
+real = 0.44e3
+
+print (real)
+
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+
+#suma
+print (a + b)
+
+#resta
+print (c - a)
+
+#multiplicacion
+print (d* c)
+
+#exponente
+print (c ** 2)
+
+#divicion
+print (c / a)
+
+#divicion entera
+print (7 / 3)
+
+#modulo
+print (7 % 3)
